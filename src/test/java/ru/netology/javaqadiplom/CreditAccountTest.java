d08513615762231bbb4cc68c0ee6cc8ca000d36d
Katerina Voroshilova <kotovaekaterina1@gmail.com>
2023-09-06 13:20:14 +0300

    Tests

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -65,7 +65,7 @@ public class CreditAccountTest {
                 15
         );
 
-        Assertions.assertEquals(true, account.pay(3_000));
+        Assertions.assertEquals(true, account.pay(4999));
     }
 
     @Test
@@ -76,7 +76,7 @@ public class CreditAccountTest {
                 15
         );
 
-        Assertions.assertEquals(false, account.pay(-3_000));
+        Assertions.assertEquals(false, account.pay(-1));
     }
 
     @Test
@@ -101,6 +101,142 @@ public class CreditAccountTest {
         Assertions.assertEquals(true, account.pay(5_000));
     }
 
+    @Test
+    public void shouldNotPayAmountNull() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(false, account.pay(0));
+    }
+
+    @Test
+    public void shouldDoublePay() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        account.pay(2000);
+        account.pay(1000);
+
+        Assertions.assertEquals(-3000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayAmountWithInitialBalanceNotNull() {
+        CreditAccount account = new CreditAccount(
+                1_000,
+                5_000,
+                15
+        );
+
+        account.pay(2000);
+
+        Assertions.assertEquals(-1000, account.getBalance());
+    }
+
+    @Test
+    public void shouldAdd() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(true, account.add(1));
+    }
+
+    @Test
+    public void shouldNotAddAmountLetterZero() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(false, account.add(-1));
+    }
+
+    @Test
+    public void shouldNotAddAmountEqualsZero() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(false, account.add(0));
+    }
+
+    @Test
+    public void shouldDoubleAdd() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        account.add(1);
+        account.add(1);
+
+        Assertions.assertEquals(2, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddWithNotNullInitialBalance() {
+        CreditAccount account = new CreditAccount(
+                1,
+                5_000,
+                15
+        );
+
+        account.add(1);
+
+        Assertions.assertEquals(2, account.getBalance());
+    }
+
+
+    @Test
+    public void shouldYearChange() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        account.pay(200);
+
+        Assertions.assertEquals(-30, account.yearChange());
+    }
+
+    @Test
+    public void shouldNotYearChange() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        account.add(200);
+
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+    @Test
+    public void shouldYearChangeNull() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+
+        Assertions.assertEquals(0, account.yearChange());
+    }
 
 
 }
