3836bbaa2b4752260944bc53531ca8de4b22565f
NetoTest <n.s.gatsu16@gmail.com>
2023-09-07 19:27:24 +0300

    доб. тест с отриц балансом

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -65,9 +65,9 @@ public class SavingAccountTest {
     public void minBalanceMoreMaxBalance() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
                     SavingAccount account = new SavingAccount(
-                            2_000,
-                            15_000,
+                            -2_000,
                             6_000,
+                            15_000,
                             12
                     );
                 });
