e215e369b674370dacc4878ecb82ccdb46144e2b
NetoTest <n.s.gatsu16@gmail.com>
2023-09-11 23:18:02 +0300

    chanched

--- src/test/java/ru/netology/javaqadiplom/BankTest.java
+++ src/test/java/ru/netology/javaqadiplom/BankTest.java
@@ -23,9 +23,15 @@ public class BankTest {
     );
 
     SavingAccount account4 = new SavingAccount(
+<<<<<<< Updated upstream
             4_000,
             4_000,
             6_000,
+=======
+            7_000,
+            4_000,
+            13_000,
+>>>>>>> Stashed changes
             5
     );
 
@@ -36,11 +42,49 @@ public class BankTest {
             5
     );
 
+<<<<<<< Updated upstream
     @Test
     public void test() {
         Bank bank = new Bank();
 
         Assertions.assertEquals(false,bank.transfer(account4, account5,5_000));
+=======
+    SavingAccount account6 = new SavingAccount(
+            6_000,
+            5_000,
+            11_000,
+            5
+    );
+
+  //  @Test
+    public void TransferMoreThanTheInitialAmount() { // перевод ьольшей возможной ишу
+        Bank bank = new Bank();
+
+        Assertions.assertEquals(false,bank.transfer(account5, account4,5_000));
+    }
+    //   @Test
+    public void test() { // проверка баланса аккаунта после перевода
+        Bank bank = new Bank();
+
+        Assertions.assertEquals(true,bank.transfer(account4, account5,3_000));
+        Assertions.assertEquals(4_000,account4.getBalance());
+        Assertions.assertEquals(7_000,account5.getBalance());
+    }
+
+
+   // @Test
+    public void testThree() { // перевод на то же счет
+        Bank bank = new Bank();
+
+        Assertions.assertEquals(false,bank.transfer(account6, account6,1_000));
+    }
+
+    @Test
+    public void testFour() { // перевод отрицательной суммы
+        Bank bank = new Bank();
+
+        Assertions.assertEquals(false,bank.transfer(account6, account5,-1_000));
+>>>>>>> Stashed changes
     }
 
     @Test
@@ -48,9 +92,16 @@ public class BankTest {
         Bank bank = new Bank();
 
         Assertions.assertEquals(true,bank.transfer(account2, account1,2_000));
+<<<<<<< Updated upstream
     }
 
     @Test
+=======
+
+    }
+
+  //  @Test
+>>>>>>> Stashed changes
     public void shouldNotTransferSavingToSavingBalanceLetterMin() {
         Bank bank = new Bank();
 
